ce8c41c07225e1d20ea8733ee980367103a9a70f
Guismx <guilhermesmx@gmail.com>
2024-11-08 15:43:23 -0300

    new change

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     
 
-    <style>
+<style>
       body {
         background:linear-gradient(to right, #0c0c0c, rgb(29, 29, 29), rgb(29, 29, 29), #0c0c0c);
         color: white;
@@ -28,7 +28,7 @@
       padding: 0; 
       gap: 2rem; /* Espaçamento entre os itens */
       padding-right: 5px; /* Padding específico à direita */
-}
+      }
 
       .navbar-nav .nav-link.active {
         color: #00ffe0a6;
@@ -66,45 +66,44 @@
       padding: 20px;
       }
 
-    .active {
-      display: block;
-    }
-    .menu-home {
-      display: flex;
-      max-width: 1200px;
-      flex-direction: center;
-      margin: 0 auto;
+      .active {
+        display: block;
+      }
+      .menu-home {
+        display: flex;
+        max-width: 1200px;
+        flex-direction: center;
+        margin: 0 auto;
 
-    }
-    .menu-home a {
-      display: flex;
-      width: 400px;
-      justify-content: center;
-      align-items: center;
-      cursor: pointer;
-      height: 30px;
-    }
-    .menu-home a:hover {
-      border-bottom: 2px solid #00ffe0a6;
-    }
-      /* Estilo do container do banner */
-    .banner {
-      width: 100%; /* O banner ocupa toda a largura da página */
-      height: 400px; /* Defina a altura do banner conforme necessário */
-      display: flex; /* Usa Flexbox para centralizar o conteúdo dentro do banner */
-      justify-content: center; /* Centraliza horizontalmente */
-      align-items: center; /* Centraliza verticalmente */
-      overflow: hidden; /* Garante que qualquer parte da imagem que ultrapasse seja oculta */
-    }
-
-    /* Estilo da imagem dentro do banner */
-    .banner img {
-      max-width: 1200px;
-      width: 1200px;  /* A imagem vai cobrir toda a largura do banner */
-      height: 100%; /* A imagem vai cobrir toda a altura do banner */
-      object-fit: cover; /* Faz com que a imagem cubra a área sem distorcer */
-    }
-    </style>
+      }
+      .menu-home a {
+        display: flex;
+        width: 400px;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+        height: 30px;
+      }
+      .menu-home a:hover {
+        border-bottom: 2px solid #00ffe0a6;
+      }
+        /* Estilo do container do banner */
+      .banner {
+        width: 100%; /* O banner ocupa toda a largura da página */
+        height: 400px; /* Defina a altura do banner conforme necessário */
+        display: flex; /* Usa Flexbox para centralizar o conteúdo dentro do banner */
+        justify-content: center; /* Centraliza horizontalmente */
+        align-items: center; /* Centraliza verticalmente */
+        overflow: hidden; /* Garante que qualquer parte da imagem que ultrapasse seja oculta */
+      }
+      /* Estilo da imagem dentro do banner */
+      .banner img {
+        max-width: 1200px;
+        width: 1200px;  /* A imagem vai cobrir toda a largura do banner */
+        height: 100%; /* A imagem vai cobrir toda a altura do banner */
+        object-fit: cover; /* Faz com que a imagem cubra a área sem distorcer */
+      }
+</style>
 
 </head>
 <body>
@@ -119,7 +118,7 @@
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
             <a class="nav-link active" aria-current="page" href="#servicos">Serviços</a>               
-            <a class="nav-link" href="#" role="button" aria-expanded="false">Médicos</a>
+            <a class="nav-link" href="#" role="button" aria-expanded="false">Equipe</a>
             <a class="nav-link" href="#" role="button" aria-expanded="false">Localização</a>
             <a class="nav-link" href="#" role="button" aria-expanded="false">Sobre</a>
             <a class="nav-link" href="login.php">Login</a>
@@ -205,8 +204,19 @@
   </div>
 </div>
 
-
-
+<div class="container-equipe"> 
+  <div class="banner-equipe">
+    <div class="dado-equipe">
+      <img src="./assets/consulta-geral.png" alt="">
+      <h1>Nome</h1>
+      <h5>Especialidade</h5>
+      <h5>CRM</h5>
+    </div>
+    <div class="infor-equipe">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, accusamus autem modi eos quisquam tenetur consequuntur. Delectus libero dolorem earum velit sint aperiam? Tenetur repellendus numquam provident quos itaque ipsam.</p>
+    </div>
+  </div>
+</div>
 
 
 
