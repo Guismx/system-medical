9970f28e8184a28bdc350ea453a200f75abd03c0
Guismx <guilhermesmx@gmail.com>
2024-11-12 10:01:49 -0300

    only test

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MGC Clinic</title>
-    <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">    
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    
+    <link rel="stylesheet" href="css/styles.css">    
 <style>
       .navbar-nav .nav-link.active {
         color: #00ffe0a6;
